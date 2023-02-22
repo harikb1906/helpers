9ca256893abb106396f5cc98d874374d31123639
HarikB <47782671+harikb1906@users.noreply.github.com>
2023-02-22 09:31:12 +0530

    Create my_decorators.py, logger added

---
+++ my_decorators.py
@@ -0,0 +1,7 @@
+def logger(function):
+    def wrapper(*args, **kwargs):
+        print(f"----- {function.__name__}: start -----")
+        output = function(*args, **kwargs)
+        print(f"----- {function.__name__}: end -----")
+        return output
+    return wrapper
